9525974d01d20e497d82945a817ffca522f0fa17
Cyril David <cyx@cyx.is>
2014-08-13 11:54:48 -0700

    Add ability to do setMaxListeners for the socket
    - Allow passing of `maxListeners`
    
      In cases where you're doing an async parallel or even
      a loop over a large amount, you'll get an event emitter
      error (possibly memory leak error).
    
      YMMV, so users should adjust the value accordingly.
    
      The test shows an example of this.
    
    - Add synactic sugar for QUIT

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var hiredis = require('hiredis');
 var reader = new hiredis.Reader();
 
-function connect() {
-    var sock = hiredis.createConnection.apply(hiredis, arguments);
+// Usage:
+//
+//     var redic = require('redic.js');
+//
+//     // Common style
+//     var client = redic.connect(6379, 'localhost', { maxListeners: 1000 });
+//
+//     // If you're going to use the connection for massively
+//     // parallel stuff, you'll probably need to increase
+//     // maxListeners like so:
+//     redic.connect(6379, 'localhost', { maxListeners: 1000 });
+//
+function connect(port, host, options) {
+    var sock = hiredis.createConnection(port, host);
+
+    if (options && options.maxListeners) {
+        sock.setMaxListeners(options.maxListeners);
+    }
 
     function call(args, fn) {
         fn = fn || function() {}
@@ -18,7 +34,11 @@ function connect() {
         sock.write.apply(sock, args);
     }
 
-    return Object.create({ call: call });
+    function quit() {
+        call(['QUIT']);
+    }
+
+    return Object.create({ call: call, quit: quit });
 }
 
 module.exports = {
